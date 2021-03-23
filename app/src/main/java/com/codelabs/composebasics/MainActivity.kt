d4f10a037d322e2f183f99be70e23ac726da5b9b
alfonsorojasrvz <nanoyv6@gmail.com>
2021-03-23 23:39:19 +0100

    Animate as a state

--- app/src/main/java/com/codelabs/composebasics/MainActivity.kt
+++ app/src/main/java/com/codelabs/composebasics/MainActivity.kt
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
@@ -14,9 +17,7 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Surface
 import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,7 +49,14 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    var isSelected by remember{ mutableStateOf(false)}
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+
+    Text(text = "Hello $name!", modifier = Modifier
+        .padding(24.dp)
+        .background(color = backgroundColor)
+        .clickable(onClick = { isSelected = !isSelected })
+    )
 }
 
 @Preview(showBackground = true)
